fbce320e9370e2968262f99bc27b348240479d2f
gauamchaudhari <gautam.chaudhari.5317@gmail.com>
2024-03-10 14:34:21 +0530

    Docker setup issue resolved

--- database/seeds/005_role_permission_seeder.js
+++ database/seeds/005_role_permission_seeder.js
@@ -7,9 +7,7 @@ const { tables } = require('../../constants/table.constants');
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex(tables.ROLE_PERMISSIONS).del()
-  await knex(tables.ROLE_PERMISSIONS).insert([
-    { id: 1, colName: 'rowValue1' },
-    { id: 2, colName: 'rowValue2' },
-    { id: 3, colName: 'rowValue3' }
-  ]);
+  // await knex(tables.ROLE_PERMISSIONS).insert([
+  
+  // ]);
 };
